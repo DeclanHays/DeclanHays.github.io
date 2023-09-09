3d5dc650c742657d23f7e8db1ee21eccdb99cbf6
Declan Hays <declanshays01@gmail.com>
2023-09-09 15:59:43 -0400

    Adding resume

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
-import { useColorMode, useMediaQuery, Circle, Flex, Box, Text, Stack, Button, Image, HStack } from '@chakra-ui/react';
+import { useColorMode, useMediaQuery, Circle, Flex, Box, Text, Stack, Button, Image, HStack, Center, IconButton } from '@chakra-ui/react';
 import React from 'react';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 function Header() {
 
@@ -9,38 +10,37 @@ function Header() {
     const [isNotSmallerScreen] = useMediaQuery("(min-width:600px");
 
     return (
-        <Stack>
-            <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"} mb={isNotSmallerScreen ? "0" : "8"} 
-                    borderRadius="full" backgroundColor="white" opacity="1" boxShadow="lg" boxSize="300px"
-                    src="https://avatars.githubusercontent.com/u/48809634?s=400&u=f21ca36dbdefafd12e65b49446ad6df8dd2111a9&v=4"></Image>
-            <Text mb="0" align="center" fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Declan Hays</Text>
+        <Center>
+            <Box display='flex' h='100vh' pb={8}>
+                <Stack flex='1'>
+                    <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "4"} mb={isNotSmallerScreen ? "0" : "4"} 
+                            borderRadius="full" backgroundColor="white" opacity="1" boxShadow="lg" boxSize="300px"
+                            src="https://avatars.githubusercontent.com/u/48809634?s=400&u=f21ca36dbdefafd12e65b49446ad6df8dd2111a9&v=4"></Image>
+                    <Text mb="0" align="center" fontSize="7xl" fontWeight="bold" bgGradient="linear(to-r, cyan.400, blue.500, purple.600)" bgClip="text">Declan Hays</Text>
 
-            <Flex direction={isNotSmallerScreen ? "row" : "column"} 
-                spacing="200px" 
-                p={isNotSmallerScreen ? "1" : "0"} 
-                alignSelf="center">
-                
-                <Box mt={isNotSmallerScreen ? "0" : 8} pb="600px" align="flex-start" alignItems="center">
-                    <Text fontSize="xl" align="center" color={isDark ? "gray.200" : "gray.500"}>
-                        Computer Science student at the University of Maryland
-                    </Text>
-                    <Text fontSize="xl" align='center' color={isDark ? "gray.200" : "gray.500"}>
-                        Previously interned at Practice Fusion
-                    </Text>
-                </Box>
-                
-            </Flex>
-            
-            <Flex justifyContent='center' flexWrap='wrap'>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-                <Button alignSelf='center' width="50" m={2} colorScheme="blue" onClick={() => window.open("https://google.com")} align='center'>Hire Me</Button>
-            </Flex>
-        </Stack>
+                    <Flex flex='1' direction={isNotSmallerScreen ? "row" : "column"} 
+                        spacing="200px" 
+                        p={isNotSmallerScreen ? "1" : "0"} 
+                        alignSelf="center">
+                        
+                        <Box mt={isNotSmallerScreen ? "0" : 8} align="flex-start" alignItems="center">
+                            <Text fontSize="xl" align="center" color={isDark ? "gray.200" : "gray.500"}>
+                                Computer Science student at the University of Maryland
+                            </Text>
+                            <Text fontSize="xl" align='center' color={isDark ? "gray.200" : "gray.500"}>
+                                Previously interned at Practice Fusion
+                            </Text>
+                        </Box>
+
+                    </Flex>
+
+                    <Flex justifyContent='center' flexWrap='wrap' flex='1'>
+                        <IconButton alignSelf='center' width="50" m={2} icon={<FaLinkedin />} onClick={() => window.open('https://www.linkedin.com/in/declan-hays/')}></IconButton>
+                        <IconButton alignSelf='center' width="50" m={2} icon={<FaGithub />} onClick={() => window.open('https://github.com/DeclanHays')}></IconButton>
+                    </Flex>
+                </Stack>
+            </Box>
+        </Center>
     )
 }
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,4 +1,4 @@
-import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex, HStack, ListItem, Text, UnorderedList, VStack, useMediaQuery, IconButton, Icon, Spacer } from '@chakra-ui/react';
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Flex, HStack, ListItem, Text, UnorderedList, Stack, useMediaQuery, IconButton, Icon, Spacer } from '@chakra-ui/react';
 import React from 'react';
 import { FaMoon, FaTerminal } from 'react-icons/fa';
 
@@ -11,14 +11,15 @@ function Resume() {
 
 
     return (
-        <Flex mt="150px" flexDirection='row' ml='8'>
-            <Accordion allowToggle allowMultiple>
+        <Flex flexDirection='row' ml='8'>
+            <Accordion allowToggle allowMultiple width="100vh" flex='1'>
                 <AccordionItem>
                     <h>
                         <AccordionButton>
-                            <Box as='span' textAlign='left'>
-                                <Text fontSize="4xl" fontWeight='bold' alignSelf="flex-start" ml=''>About Me</Text>
+                            <Box as='span' alignSelf='center' flex='1'>
+                                <Text fontSize="4xl" fontWeight='bold'  ml=''>About Me</Text>
                             </Box>
+                            <AccordionIcon />
                         </AccordionButton>
                     </h>
                     <AccordionPanel pb={4}>
@@ -26,10 +27,16 @@ function Resume() {
                             <Box>
                                 <UnorderedList>
                                     <ListItem>
-                                        Item 1
+                                        I am currently in my senior year at the University of Maryland, College Park, pursuing my B.S. in Computer Science.
                                     </ListItem>
                                     <ListItem>
-                                        Item 2
+                                        During summer of 2023, I gained experience in front-end software engineering through my internship at Veradigm. 
+                                    </ListItem>
+                                    <ListItem>
+                                        My professional interests are in front-end, back-end, and full-stack software development, as well as data science and A.I. 
+                                    </ListItem>
+                                    <ListItem>
+                                    Previously, I have worked at a boat rental company where I fulfilled duties as a captain, customer service rep, and performed maintenance on the boats.
                                     </ListItem>
                                 </UnorderedList>
                             </Box>
@@ -41,38 +48,51 @@ function Resume() {
                 <AccordionItem>
                     <h>
                         <AccordionButton>
-                            <Box as='span' textAlign='left'>
+                            <Box as='span' flex='1'>
                                 <Text fontSize="4xl" fontWeight='bold' alignSelf="flex-start" ml=''>Education</Text>
                             </Box>
+                            <AccordionIcon />
                         </AccordionButton>
                     </h>
                     <AccordionPanel pb={4}>
-                        <HStack>
-                            <Box>
+                        <Flex flexWrap='wrap'>
+                            <Box w='375px'>
                                 <Text fontSize='xl' fontWeight='bold' >University of Maryland, College Park</Text>
                                 <Text fontSize='l' >GPA: 3.48/4.00</Text>
                                 <Text>Relevant Coursework</Text>
                                 <UnorderedList>
-                                    <ListItem>Class 1</ListItem>
+                                    <ListItem>Current: CMSC 421: Intro to Artificial Intelligence (Python)</ListItem>
+                                    <ListItem>Current: CMSC 320: Intro to Data Science (Python)</ListItem>
+                                    <ListItem>CMSC 433: Programming Language Technologies and Paradigms (Haskell)</ListItem>
+                                    <ListItem>CMSC 420: Advanced Data Structures (Python)</ListItem>
+                                    <ListItem>CMSC 351: Algorithms</ListItem>
+                                    <ListItem>CMSC 335: Web Development w/ JavaScript (JavaScript, HTML, CSS)</ListItem>
+                                    <ListItem>CMSC 330: Organization of Programming Languages (OCaml, Ruby, Rust)</ListItem>
+                                    <ListItem>CMSC 250: Discrete Math</ListItem>
+                                    <ListItem>CMSC 216: Intro to Computer Systems (C)</ListItem>
+                                    <ListItem>CMSC 133: Object Oriented Programming (Java)</ListItem>
                                 </UnorderedList>
                             </Box>
-                            <Box ml='14'>
+                            <Spacer />
+                            <Box w='350px'>
                                 <Text fontSize='xl' fontWeight='bold' >Anne Arundel Community College</Text>
                                 <Text fontSize='l' >GPA: 3.93/4.00</Text>
                                 <Text>Relevant Coursework</Text>
                                 <UnorderedList>
-                                    <ListItem>Class 1</ListItem>
+                                    <ListItem>Intro to Data Science</ListItem>
+                                    <ListItem>Object Oriented Programming w/ Java</ListItem>
                                 </UnorderedList>
                             </Box>
-                            <Box ml='14'>
+                            <Spacer />
+                            <Box w='350px'>
                                 <Text fontSize='xl' fontWeight='bold' >South River High School</Text>
-                                <Text fontSize='l' >GPA: 3.93/4.00</Text>
+                                <Text fontSize='l' >GPA: 3.90/4.00</Text>
                                 <Text>Relevant Coursework</Text>
                                 <UnorderedList>
-                                    <ListItem>Class 1</ListItem>
+                                    <ListItem>A.P Computer Science A</ListItem>
                                 </UnorderedList>
                             </Box>
-                        </HStack>
+                        </Flex>
                     </AccordionPanel>
                 </AccordionItem>
 
@@ -80,17 +100,52 @@ function Resume() {
                 <AccordionItem>
                     <h>
                         <AccordionButton>
-                            <Box as='span' textAlign='left'>
+                            <Box as='span' flex='1'>
                                 <Text fontSize="4xl" fontWeight='bold' alignSelf="flex-start" ml=''>Experience</Text>
                             </Box>
+                            <AccordionIcon />
                         </AccordionButton>
                     </h>
                     <AccordionPanel pb={4}>
-                        <HStack>
+                        <Stack>
                             <Box>
-                                <Text>Veradigm</Text>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Veradigm</Text>
+                                    <Text fontSize='l'>| Software Engineering Intern | May 2023 - Aug 2023</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Created a testing suite using JavaScript and Puppeteer for validating component styles.</ListItem>
+                                    <ListItem>Expanded accessibility for pre-existing Ember.js components using CSS classes.</ListItem>
+                                    <ListItem>Integrated third-party accessibility-auditing tool into codebase. Used library to test for, and solve, accessibility errors in e-prescribe workflow.</ListItem>
+                                    <ListItem>Implemented accessible components into e-prescribe workflow in Practice Fusion using HTML.</ListItem>
+                                    <ListItem>Wrote unit, integration, and acceptance tests to cover new components added to codebase.</ListItem>
+                                    <ListItem>Participated in code reviews and provided constructive feedback to team members.</ListItem>
+                                </UnorderedList>
                             </Box>
-                        </HStack>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>South River Boat Rentals</Text>
+                                    <Text fontSize='l'>| Captain/Customer Service Representative</Text>
+                                    <Text fontSize='l'>| May 2018 - Aug 2022</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Led customers on 4-to-8-hour excursions on the Chesapeake Bay.</ListItem>
+                                    <ListItem>Taught customers to drive boats safely and to adhere to coast guard regulations.</ListItem>
+                                    <ListItem>Worked with team to safely guide dozens of boats to and from docks.</ListItem>
+                                </UnorderedList>
+                            </Box>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Anne Arundel Community College</Text>
+                                    <Text fontSize='l'>| Data Science Tutor</Text>
+                                    <Text fontSize='l'>| Oct 2021 - May 2022</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Instructed novice programmers in Python and guided debugging.</ListItem>
+                                    <ListItem>Assisted math students with coursework up to Calculus I.</ListItem>
+                                </UnorderedList>
+                            </Box>
+                        </Stack>
                     </AccordionPanel>
                 </AccordionItem>
 
@@ -98,24 +153,48 @@ function Resume() {
                 <AccordionItem>
                     <h>
                         <AccordionButton>
-                            <Box as='span' textAlign='left'>
+                            <Box as='span' flex='1'>
                                 <Text fontSize="4xl" fontWeight='bold' alignSelf="flex-start" ml=''>Skills</Text>
                             </Box>
+                            <AccordionIcon />
                         </AccordionButton>
                     </h>
                     <AccordionPanel pb={4}>
-                        <HStack>
-                            <Box>
+                        <Flex flexWrap='wrap' direction={isNotSmallerScreen ? 'row' : 'column'}>
+                            <Box marginRight='50px' pb={2}>
+                                <Text fontSize="xl" fontWeight={'bold'}>Programming Languages</Text>
                                 <UnorderedList>
-                                    <ListItem>
-                                        None
-                                    </ListItem>
-                                    <ListItem>
-                                        Item 2
-                                    </ListItem>
+                                    <ListItem>JavaScript</ListItem>
+                                    <ListItem>Python</ListItem>
+                                    <ListItem>Java</ListItem>
+                                    <ListItem>C</ListItem>
+                                    <ListItem>OCaml</ListItem>
+                                    <ListItem>Haskell</ListItem>
                                 </UnorderedList>
                             </Box>
-                        </HStack>
+                            
+                            <Box marginRight='50px' pb={2}>
+                                <Text fontSize="xl" fontWeight={'bold'}>Web Development</Text>
+                                <UnorderedList>
+                                    <ListItem>HTML</ListItem>
+                                    <ListItem>CSS</ListItem>
+                                    <ListItem>Ember</ListItem>
+                                    <ListItem>Express</ListItem>
+                                </UnorderedList>
+                            </Box>
+                            
+                            <Box marginRight='50px' pb={2}> 
+                                <Text fontSize="xl" fontWeight={'bold'}>Other</Text>
+                                <UnorderedList>
+                                    <ListItem>Git</ListItem>
+                                    <ListItem>Node.js</ListItem>
+                                    <ListItem>Jira</ListItem>
+                                    <ListItem>Linux</ListItem>
+                                    <ListItem>Handlebars</ListItem>
+                                    <ListItem>MongoDB</ListItem>
+                                </UnorderedList>
+                            </Box>
+                        </Flex>
                     </AccordionPanel>
                 </AccordionItem>
 
@@ -123,24 +202,79 @@ function Resume() {
                 <AccordionItem>
                     <h>
                         <AccordionButton>
-                            <Box as='span' textAlign='left'>
+                            <Box as='span' flex='1'>
                                 <Text fontSize="4xl" fontWeight='bold' alignSelf="flex-start" ml=''>Projects</Text>
                             </Box>
+                            <AccordionIcon />
                         </AccordionButton>
                     </h>
                     <AccordionPanel pb={4}>
-                        <HStack>
+                        <Stack>
                             <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Personal Website</Text>
+                                    <Text fontSize='l'>| JavaScript, HTML, CSS, React</Text>
+                                    <Text fontSize='l'>| Aug 2023 - Sep 2023</Text>
+                                </HStack>
                                 <UnorderedList>
-                                    <ListItem>
-                                        Also none
-                                    </ListItem>
-                                    <ListItem>
-                                        Item 2
-                                    </ListItem>
+                                    <ListItem>Used React and ChakraUI to make a personal portfolio website.</ListItem>
                                 </UnorderedList>
                             </Box>
-                        </HStack>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Jose's DVD Emporium</Text>
+                                    <Text fontSize='l'>| Haskell</Text>
+                                    <Text fontSize='l'>| May 2023</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Created a specialized data structure, an STM queue, for use in concurrent programs using the Software Transactional Memory model.</ListItem>
+                                    <ListItem>Wrote the back end for a hypothetical DVD delivery service which uses the STM queue to schedule concurrent “drivers” to “deliver” DVD's in order, as they are ordered.</ListItem>
+                                </UnorderedList>
+                            </Box>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Data Structures</Text>
+                                    <Text fontSize='l'>| Python</Text>
+                                    <Text fontSize='l'>| Jan 2023 - May 2023</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Used Python to implement various data structures including Scapegoat Trees, B+ Trees, Bloom Filters, AVL Trees, and Binary Search Trees..</ListItem>
+                                </UnorderedList>
+                            </Box>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>MicrOCaml</Text>
+                                    <Text fontSize='l'>| OCaml</Text>
+                                    <Text fontSize='l'>| Nov 2023</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Used OCaml to write “MicrOCaml”: a lexer, parser, and interpreter for a miniature version of OCaml. MicrOCaml allows the user to write programs which are fully processed and executed by MicrOCaml.</ListItem>
+                                    <ListItem>Includes capabilities for handling “let” and “if” expressions, function definitions, anonymous functions, and binary operators.</ListItem>
+                                </UnorderedList>
+                            </Box>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Summer Camp App</Text>
+                                    <Text fontSize='l'>| JavaScript, HTML, CSS</Text>
+                                    <Text fontSize='l'>| Nov 2022</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Designed a web application, written in JavaScript, for a fictional summer camp.</ListItem>
+                                    <ListItem>Utilized MongoDB to store and retrieve applicant data upon request and used Express for routing.</ListItem>
+                                </UnorderedList>
+                            </Box>
+                            <Box>
+                                <HStack>
+                                    <Text fontSize='xl' fontWeight={'bold'}>Regular Expression Engine</Text>
+                                    <Text fontSize='l'>| OCaml</Text>
+                                    <Text fontSize='l'>| Oct 2022</Text>
+                                </HStack>
+                                <UnorderedList>
+                                    <ListItem>Created an engine which allows the user to convert between regular expressions, non-deterministic finite automata, and deterministic finite automata using OCaml. </ListItem>
+                                    <ListItem>Allows the user to provide a regular expression and test if given inputs are valid. 	</ListItem>
+                                </UnorderedList>
+                            </Box>
+                        </Stack>
                     </AccordionPanel>
                 </AccordionItem>
                 
